c67631d4892bddbb38014d421ddcae4f0cc40b75
PedroAC18 <pedro.correa1101@gmail.com>
2023-05-05 15:34:50 -0300

    Improvement on the UI

--- Game_System/src/Application/UI.java
+++ Game_System/src/Application/UI.java
@@ -33,6 +33,12 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    //clear console after each movement
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
 
     //reading the position that the user digits
     public static ChessPosition readChessPosition(Scanner sc){
